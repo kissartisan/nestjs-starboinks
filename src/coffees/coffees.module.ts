51aba517c24b5c30a5ff8a0a534ac12338e7dfe3
Reymark Torres <rtorres@linktr.ee>
2022-10-27 16:04:11 +0800

    Create a Relation between two Entities

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entity/coffee.entity';
+import { Flavor } from './entity/flavor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
