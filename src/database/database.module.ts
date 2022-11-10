f806fe775494a700c6e88c98e3aa32c06edaddad
Reymark Torres <rtorres@linktr.ee>
2022-11-10 21:37:07 +0800

    DI > DB module > Add comments for learning purposes

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,11 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
+/*
+  👉👉👉 NOTE: If you are following along with this course and going to keep this dynamic module...
+
+  ⭐⭐⭐⭐ ️️Make sure you pass in "username" and "password".
+*/
 @Module({})
 export class DatabaseModule {
   static register(options: DataSourceOptions): DynamicModule {
